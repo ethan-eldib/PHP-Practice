68d01d08a3201b91f237d34e73369f27f1a0844e
unknown <ethan.eldib@gmail.com>
2023-02-27 11:29:25 +0100

    feature(exerice3): Suggested correction for exercise 3

--- correction/exercice3.php
+++ correction/exercice3.php
@@ -1,1 +1,13 @@
 
+// La fonction
+function isPalindrome ($word): string
+{
+    if ($word === strrev($word)) {
+        return "C'est bien un Palindrome, le mot saisi est " . strtoupper($word) . " et une fois inversé cela donne " . strtoupper(strrev($word));
+    } else {
+        return "Ce n'est pas un Palindrome, le mot saisi est " . strtoupper($word) . " et une fois inversé cela donne " . strtoupper(strrev($word));
+    }
+}
+
+// L'affichage
+isPalindrome("radar")
