1766b0a7f1851b49a350bc43444f4d19e4804528
unknown <ethan.eldib@gmail.com>
2023-02-28 07:15:45 +0100

    feature(exerice8): Suggested correction for exercise 8

---
+++ correction/exercice8.php
@@ -0,0 +1,24 @@
+// Première proposition
+function getEvenNumbers (array $numbers): array
+{
+    $evenNumbers = [];
+    foreach ($numbers as $number) {
+        if ($number%2 === 0) {
+            $evenNumbers[] = $number;
+        }
+    }
+
+    return $evenNumbers;
+}
+
+// Seconde proposition avec array_filter()
+function getEvenNumbers (array $numbers): array
+{
+    return array_filter($numbers, function ($number) {
+        return $number % 2 === 0;
+    });
+}
+
+// Appel de la fonction
+"Liste des nombres pairs saisis dans le tableau : ". implode(", ", getEvenNumbers([12,10,8,1,7,9]))
+
